4c0b94a95e018fc282f6518daca8e15e92671415
Shah <shebiraaja@gmail.com>
2022-04-09 16:08:54 +0200

    started  styling the home page

--- src/portfolio/home/Profile.js
+++ src/portfolio/home/Profile.js
@@ -1,57 +1,76 @@
 import React from 'react'
 import Typical from 'react-typical'
+import '../home/Profile.css'
 
 export default function Profile() {
-  return (
-    <div className='profile-container'>
-        <div className='profile-parent'>
-            <div className='profile-detail'>
-                <div className='icon-column'>
-                    <a href="#">
-                        <i class="fa-brands fa-facebook"></i>
-                    </a>
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-detail'>
+                    <div className='icon-column'>
+                        <div className="all-icons">
+                            <a href="#">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
 
-                    <a href="#">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
+                            <a href="#">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
 
-                    <a href="#">
-                        <i class="fa-brands fa-twitter"></i>
-                    </a>
+                            <a href="#">
+                                <i class="fa-brands fa-twitter"></i>
+                            </a>
 
-                    <a href="#">
-                        <i class="fa-solid fa-envelope"></i>
-                    </a>
-                </div>
+                            <a href="#">
+                                <i class="fa-solid fa-envelope"></i>
+                            </a>
+                        </div>
+                    </div>
 
-                <div className='profile-details-name'>
-                    <span className='primary-text'>
-                    
-                        Hello, I'M <span className='highlighted-text'>Shah Zaib</span>
-                    </span>
-                </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
 
-                <div className='profile-details-profesion'>
-                    <span>
-                        <h1>
-                            <Typical
-                            loop={Infinity}
-                            steps={[
-                                "Frontend Developer",
-                                1000,
-                                "Wordpress Designer",
-                                1000,
-                                "React/Bootstrap Developer",
-                                1000,
-                                "Specialist in Networking",
-                                1000,
-                            ]}
-                            />
-                        </h1>
-                    </span>
+                            Hello, I'M <span className='highlighted-text'>Shah Zaib</span>
+                        </span>
+                    </div>
+
+                    <div className='profile-details-profesion'>
+                        <span>
+                            <h1>
+                                <Typical
+                                    loop={Infinity}
+                                    steps={[
+                                        "Frontend Developer",
+                                        1000,
+                                        "Wordpress Expert",
+                                        1000,
+                                        "React/Bootstrap Developer",
+                                        1000,
+                                        "Specialist in Networking",
+                                        1000,
+                                    ]}
+                                />
+                            </h1>
+                        </span>
+
+                        <span className='shortDesc'>
+                            Knack of building websites with frontend operations.
+                        </span>
+                    </div>
+                    <div className='mt-4'>
+                        <button>Hire Me!</button>
+
+                        <a href="shebi.pdf" download="Shah Zaib.pdf">
+                            <button>Get CV</button>
+                        </a>
+                    </div>
+
+                    <div className="profile-picture">
+
+                    </div>
+
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
