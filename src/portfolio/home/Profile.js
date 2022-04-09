fe74f826b17d37a3ff87780ccfeb03ce1e24b0d5
Shah <shebiraaja@gmail.com>
2022-04-09 15:06:49 +0200

    designing typical animation titles

--- src/portfolio/home/Profile.js
+++ src/portfolio/home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
   return (
@@ -9,6 +10,45 @@ export default function Profile() {
                     <a href="#">
                         <i class="fa-brands fa-facebook"></i>
                     </a>
+
+                    <a href="#">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+
+                    <a href="#">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+
+                    <a href="#">
+                        <i class="fa-solid fa-envelope"></i>
+                    </a>
+                </div>
+
+                <div className='profile-details-name'>
+                    <span className='primary-text'>
+                    
+                        Hello, I'M <span className='highlighted-text'>Shah Zaib</span>
+                    </span>
+                </div>
+
+                <div className='profile-details-profesion'>
+                    <span>
+                        <h1>
+                            <Typical
+                            loop={Infinity}
+                            steps={[
+                                "Frontend Developer",
+                                1000,
+                                "Wordpress Designer",
+                                1000,
+                                "React/Bootstrap Developer",
+                                1000,
+                                "Specialist in Networking",
+                                1000,
+                            ]}
+                            />
+                        </h1>
+                    </span>
                 </div>
             </div>
         </div>
