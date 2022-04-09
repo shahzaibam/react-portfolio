3cd668143097b1273a3626743905bd67c2b0810b
Shah <shebiraaja@gmail.com>
2022-04-09 14:38:03 +0200

    creado ficheros actualizando los iconos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './portfolio/home/Profile';
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+
 
 function App() {
   return (
-
+    <Profile/>
   );
 }
 
---
+++ src/portfolio/home/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-detail'>
+                <div className='icon-column'>
+                    <a href="#">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
